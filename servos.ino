97e88f09cf1e70d11384349664a9ade9b8d7a541
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2022-06-05 22:40:30 +0200

    Update servos.ino

--- servos.ino
+++ servos.ino
@@ -10,26 +10,26 @@
 
 #include <Servo.h>
 
-#define s1center 85 // posiciones de centrado de los servos
+#define s1center 86 // posiciones de centrado de los servos
 #define s3center 95
-#define s4center 100
-#define s6center 80
+#define s4center 95
+#define s6center 95
 
-#define s1giro 135 // posiciones de giro
-#define s3giro 45
+#define s1giro 140 // posiciones de giro
+#define s3giro 37
 #define s4giro 150
-#define s6giro 35
+#define s6giro 40
 
 Servo servo1;
 Servo servo3;
 Servo servo4;
 Servo servo6;
 
-#define D1 3
-#define D2 4
-#define D3 5
-#define D4 6
-boolean rotate;
+#define D1 10
+#define D2 16
+#define D3 14
+#define D4 15
+boolean rotate = 0;
 
 void setup() {
   
@@ -38,10 +38,10 @@ void setup() {
   pinMode(D3, INPUT);
   pinMode(D4, INPUT);
   
-  servo1.attach(10); //pines para servos
-  servo3.attach(11);
-  servo4.attach(8);
-  servo6.attach(9);
+  servo1.attach(3); //pines para servos
+  servo3.attach(6);
+  servo4.attach(9);
+  servo6.attach(5);
   
   servo1.write(s1center); //arranque en posicion central
   servo3.write(s3center);
@@ -52,7 +52,7 @@ void setup() {
 
 void loop() {
   
-  rotate = (read(D1) && read(D4)) || (read(D2) && read(D3));
+  boolean rotate = (read(D1) && read(D4)) || (read(D2) && read(D3));
   
   if (rotate){
     servo1.write(s1giro); //posicion giro
@@ -60,21 +60,23 @@ void loop() {
     servo4.write(s4giro);
     servo6.write(s6giro);
   }
-  elseif{
+  else
+  {
     servo1.write(s1center); //arranque en posicion central
     servo3.write(s3center);
     servo4.write(s4center);
     servo6.write(s6center);
+  }
   
 }
 
 boolean read(int pin)
 {
   boolean in = 0;
-  for (int i=0; i<10; i++)
+  for (int i=0; i<5; i++)
   {
     in = in || digitalRead(pin);
-    delay 2;
+    delay(2);
   }
-  return in
+  return in;
 }
